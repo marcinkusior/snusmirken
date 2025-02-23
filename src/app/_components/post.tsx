c5051d74e7eabceeff90f3ffb298d52b08b5ce89
marcinkusior <kusior.marcin@gmail.com>
2025-02-23 20:36:13 +0100

    remove latest post

--- src/app/_components/post.tsx
+++ src/app/_components/post.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import { useState } from "react";
-
 import { api } from "~/trpc/react";
 
 export function LatestPost() {
-  const [latestPost] = api.post.getLatest.useSuspenseQuery();
-
   const utils = api.useUtils();
   const [name, setName] = useState("");
   const createPost = api.post.create.useMutation({
@@ -18,11 +15,6 @@ export function LatestPost() {
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
       <form
         onSubmit={(e) => {
           e.preventDefault();
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,10 @@
-"use client";
 // filepath: /Users/test/Documents/Projects/snufkin/src/app/posts/page.tsx
+"use client";
 import React from "react";
 import { api } from "~/trpc/react";
 
 const PostsPage = () => {
   const { data: posts, isLoading, error } = api.post.getAll.useQuery();
-
-  // const posts = [latestPost];
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
@@ -14,7 +12,7 @@ const PostsPage = () => {
   return (
     <div className="flex flex-col items-center justify-center p-10">
       <h1 className="text-center">All Posts</h1>
-      {posts.length > 0 ? (
+      {posts && posts.length > 0 ? (
         <ul className="mt-5 flex w-[300px] flex-col justify-center gap-2">
           {posts.map((post) => (
             <li key={post.id} className="rounded-md border border-gray-300 p-4">
